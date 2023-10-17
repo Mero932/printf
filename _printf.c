51070e71b1415b2924e40e64948697ce114da2ab
Mero932 <mero93princessa@gmail.com>
2023-10-17 15:29:19 -0700

    printf project trial

--- _printf.c
+++ _printf.c
@@ -1,71 +1,164 @@
 #include <stdio.h>
 #include <stdarg.h>
-
+#include <unistd.h>
 /**
- * _printf - produces output according to a format.
- *
- * @format: character string.
- *
- * Return: count (character of strings)
- */
+* _putchar - This function prints a character using the putchar function.
+*
+* @c : character parameter
+* Return: 1 (success)
+*        -1 (failure)
+*/  
+int 
+_putchar (char x) 
+{
+  
+ 
+return (write (1, &x, 1));
 
-int _printf(const char *format, ...)
+ 
+}
+
+
+ 
+ 
+ 
+ 
+int _printf (const char *format, ...) 
 {
 va_list args;
-int count 
-count = 0;
-va_start(args, format);
+int count = 0;
+va_start (args, format);
 while (*format != '\0')
 {
 if (*format == '%')
-{
+	{
 format++;
 switch (*format)
-{
+	    {
 case 'c':
-{
-int c = va_arg(args, int);
+	      {
+int c = va_arg (args, int);
 write(1, &c, 1);
 count++;
+		
+ 
 break;
+	      
+ 
 }
+	    
+ 
 case 's':
-{
-char *s = va_arg(args, char *);
+	      
+ 
+	      {
+		
+ 
+char *s = va_arg (args, char *);
+		
+ 
 while (*s != '\0')
-{
-                            write(1, &*s, 1);
-
-                            s++;
-                            count++;
-                        }
-                        break;
-                    }
-                case '%':
-                    {
-                        putchar('%');
-                        count++;
-                        break;
-                    }
-                default:
-                    {
-                        putchar('%');
-                        putchar(*format);
-                        count += 2;
-                        break;
-                    }
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-
-        format++;
-    }
-
-    va_end(args);
-
-    return count;
+		  
+ 
+		  {
+		    
+ 
+write(1, &*s, 1);
+		    
+ 
+s++;
+		    
+ 
+count++;
+		  
+ 
 }
+		
+ 
+break;
+	      
+ 
+}
+	    
+ 
+case '%':
+	      
+ 
+	      {
+		
+ 
+_putchar ('%');
+		
+ 
+count++;
+		
+ 
+break;
+	      
+ 
+}
+	    
+ 
+default:
+	      
+ 
+	      {
+		
+ 
+_putchar ('%');
+		
+ 
+_putchar (*format);
+		
+ 
+count += 2;
+		
+ 
+break;
+	      
+ 
+}
+	    
+ 
+}
+	
+ 
+}
+      
+ 
+      else
+	
+ 
+	{
+	  
+ 
+_putchar (*format);
+	  
+ 
+count++;
+	
+ 
+}
+      
+ 
+ 
+format++;
+    
+ 
+}
+  
+ 
+ 
+va_end (args);
+  
+ 
+ 
+return count;
+
+ 
+}
+
+
+ 
+ 
+
