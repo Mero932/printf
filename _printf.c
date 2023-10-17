aef73e960829f4d0f8b0f4bcb687f6f1747f38c1
Mero932 <mero93princessa@gmail.com>
2023-10-17 15:27:17 -0700

    printf project trial

--- _printf.c
+++ _printf.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * _printf - produces output according to a format.
+ *
+ * @format: character string.
+ *
+ * Return: count (character of strings)
+ */
+
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+va_list args;
+int count 
+count = 0;
+va_start(args, format);
+while (*format != '\0')
+{
+if (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+{
+int c = va_arg(args, int);
+write(1, &c, 1);
+count++;
+break;
+}
+case 's':
+{
+char *s = va_arg(args, char *);
+while (*s != '\0')
+{
+                            write(1, &*s, 1);
 
-    va_start(args, format);
-
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-
-            switch (*format)
-            {
-                case 'c':
-                    {
-                        int c = va_arg(args, int);
-                        putchar(c);
-                        count++;
-                        break;
-                    }
-                case 's':
-                    {
-                        char *s = va_arg(args, char *);
-                        while (*s != '\0')
-                        {
-                            putchar(*s);
                             s++;
                             count++;
                         }
