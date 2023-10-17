7c0b6b6bb496dbad19f3e43a9611d42eab9f17ad
Mero932 <mero93princessa@gmail.com>
2023-10-17 15:37:05 -0700

    printf project trial

--- _printf.c
+++ _printf.c
@@ -8,21 +8,11 @@
 * Return: 1 (success)
 *        -1 (failure)
 */  
-int 
-_putchar (char x) 
+int _putchar (char x) 
 {
-  
- 
 return (write (1, &x, 1));
-
- 
 }
 
-
- 
- 
- 
- 
 int _printf (const char *format, ...) 
 {
 va_list args;
@@ -31,134 +21,50 @@ va_start (args, format);
 while (*format != '\0')
 {
 if (*format == '%')
-	{
+{
 format++;
 switch (*format)
-	    {
+{
 case 'c':
-	      {
+{
 int c = va_arg (args, int);
-write(1, &c, 1);
+_putchar(c);
 count++;
-		
- 
 break;
-	      
- 
 }
-	    
- 
 case 's':
-	      
- 
-	      {
-		
- 
+{
 char *s = va_arg (args, char *);
-		
- 
 while (*s != '\0')
-		  
- 
-		  {
-		    
- 
-write(1, &*s, 1);
-		    
- 
+{
+_putchar(*s);
 s++;
-		    
- 
 count++;
-		  
- 
 }
-		
- 
 break;
-	      
- 
 }
-	    
- 
 case '%':
-	      
- 
-	      {
-		
- 
+{
 _putchar ('%');
-		
- 
 count++;
-		
- 
 break;
-	      
- 
 }
-	    
- 
 default:
-	      
- 
-	      {
-		
- 
+{
 _putchar ('%');
-		
- 
 _putchar (*format);
-		
- 
 count += 2;
-		
- 
 break;
-	      
- 
 }
-	    
- 
 }
-	
- 
 }
-      
- 
-      else
-	
- 
-	{
-	  
- 
+else
+{
 _putchar (*format);
-	  
- 
 count++;
-	
- 
 }
-      
- 
- 
 format++;
-    
- 
 }
-  
- 
- 
 va_end (args);
-  
- 
- 
 return count;
-
- 
 }
-
-
- 
- 
-
