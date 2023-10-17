9216c23b8390b1c88bd8da76ed1e739f728746dc
Mero932 <mero93princessa@gmail.com>
2023-10-17 14:25:59 -0700

    printf project trial

--- _printf.c
+++ _printf.c
@@ -1,64 +1,61 @@
-#include <stdio.h>
-#include <stdarg.h>
-
+#include "main.h"
+/**
+ * _printf - produces output according to a format.
+ * @format: character string.
+ *
+ * Return: count (character of strings)
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-
-    va_start(args, format);
-
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-
-            switch (*format)
-            {
-                case 'c':
-                    {
-                        int c = va_arg(args, int);
-                        putchar(c);
-                        count++;
-                        break;
-                    }
-                case 's':
-                    {
-                        char *s = va_arg(args, char *);
-                        while (*s != '\0')
-                        {
-                            putchar(*s);
-                            s++;
-                            count++;
-                        }
-                        break;
-                    }
-                case '%':
-                    {
-                        putchar('%');
-                        count++;
-                        break;
-                    }
-                default:
-                    {
-                        putchar('%');
-                        putchar(*format);
-                        count += 2;
-                        break;
-                    }
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-
-        format++;
-    }
-
-    va_end(args);
-
-    return count;
+int i, j, count;
+char *str;
+i = 0;
+j = 0;
+count = 0;
+va_list args;
+va_start(args, format);
+while (format && format[i])
+{
+if (format[i] == '%')
+{
+i++;
+switch (format[i])
+{
+case 'c':
+j = va_arg(args, int);
+write(1, &j, 1);
+count++;
+break;
+case 's':
+str = va_arg(args, char *);
+if (!str)
+str = "(null)";
+while (str[j])
+{
+write(1, &str[j], 1);
+j++;
+count++;
+}
+j = 0;
+break;
+case '%':
+write(1, &format[i], 1);
+count++;
+break;
+default:
+write(1, "%", 1);
+write(1, &format[i], 1);
+count += 2;
+break;
+}
+}
+else
+{
+write(1, &format[i], 1);
+count++;
+}
+i++;
+}
+va_end(args);
+return (count);
 }
